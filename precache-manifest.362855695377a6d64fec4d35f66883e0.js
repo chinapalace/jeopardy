7f03fb581bedbf0c825f85565389940a3aac50f6
Justin McLaren <justinmclaren@Justins-MacBook-Pro.local>
2018-12-18 09:31:03 -0800

    Updates

---
+++ precache-manifest.362855695377a6d64fec4d35f66883e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "71de1848b47e20aae4bda030a89439b3",
+    "url": "/jeopardy/static/media/wrong.71de1848.jpg"
+  },
+  {
+    "revision": "0d290b6ba8c4817eb33e637235cd9868",
+    "url": "/jeopardy/static/media/correct.0d290b6b.png"
+  },
+  {
+    "revision": "1451512623c01f5b96a7",
+    "url": "/jeopardy/static/js/runtime~main.14515126.js"
+  },
+  {
+    "revision": "0cbbed1610dba00fecb5",
+    "url": "/jeopardy/static/js/main.0cbbed16.chunk.js"
+  },
+  {
+    "revision": "7a56a8cd279bce97bae1",
+    "url": "/jeopardy/static/js/1.7a56a8cd.chunk.js"
+  },
+  {
+    "revision": "0cbbed1610dba00fecb5",
+    "url": "/jeopardy/static/css/main.e37e5819.chunk.css"
+  },
+  {
+    "revision": "419e985e0260bcdede92e6edc7f6befe",
+    "url": "/jeopardy/index.html"
+  }
+];
